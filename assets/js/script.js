7f4cb2d355ea73e8698b767dc3584d96b10981c9
BenBasic <benjaminmbasic@gmail.com>
2022-05-31 22:42:04 -0400

    Fixed api key error for fetch request, now trying to use the icon data to display on the page but am recieving a type error of undefined, will need to look into it further to fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,41 @@ let citySearch = "";
 let apiKey = "7eed36f27d06c72d7bd4346eee02a6cf";
 
 function searchWeather() {
-    let queryURL = "https://api.openweathermap.org/data/3.0/onecall?q=" + "Toronto" + "&limit=5&appid=" + apiKey + "&units=metric";
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "Toronto" + "&limit=5&appid=" + apiKey + "&units=metric";
     fetch(queryURL, {
     method: 'GET',
 })
-  .then(function (response) {
+.then( response => { 
+    let date = moment().format("MM/DD/YYYY");
+    let icon = response.weather[0].icon;
+    let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    let weatherTitle = $("<h2>").html(citySearch + date);
+    
+    weatherSection.prepend(weatherTitle);
+    weatherSection.append($("<img>").attr("src", iconURL));
     return response.json();
-  })
-  .then(function (data) {
+}).then( data => {
     console.log(data);
-  });
+});
 }
+
+//   .then(function (response) {
+//     console.log(response);
+
+//     //Do something related to promise here I guess
+
+
+
+    
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log('Oh noooo!!');
+//     console.log(err);
+//   });
+// }
 
 searchWeather();
