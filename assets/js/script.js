243de5fedf7b013bbc9df03879c3408b2fb8d0d6
BenBasic <benjaminmbasic@gmail.com>
2022-06-01 00:31:41 -0400

    Fixed error with grabbing weather icon and now it correctly displays, added functionality to grab the uv index from a fetch request, grabbed data for temperature and humidity to correctly display them on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,61 @@
 let cities = [];
 let weatherSection = $("#weather-stuff");
-let citySearch = "";
+let citySearch = "Dubai";
 let apiKey = "7eed36f27d06c72d7bd4346eee02a6cf";
 
 function searchWeather() {
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "Toronto" + "&limit=5&appid=" + apiKey + "&units=metric";
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&limit=5&appid=" + apiKey + "&units=metric";
     fetch(queryURL, {
     method: 'GET',
 })
 .then( response => { 
-    let date = moment().format("MM/DD/YYYY");
-    let icon = response.weather[0].icon;
-    let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    let weatherTitle = $("<h2>").html(citySearch + date);
-    
-    weatherSection.prepend(weatherTitle);
-    weatherSection.append($("<img>").attr("src", iconURL));
     return response.json();
 }).then( data => {
     console.log(data);
+    let date = moment().format("MM/DD/YYYY");
+    let icon = data.weather[0].icon;
+    let iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
+    let weatherTitle = $("<h2>").html(citySearch + date).attr({"class": "weather title"});
+    let currentTemperature = data.main.temp;
+    let temperatureDisplay = $("<h2>").html(currentTemperature).attr({"class": "temp"});
+    let currentHumidity = data.main.humidity;
+    let humidityDisplay = $("<h2>").html(currentHumidity).attr({"class": "humidity"});
+    let lat = data.coord.lat;
+    let lon = data.coord.lon;
+    let dt = data.dt;
+    console.log(lat,lon,dt);
+    
+    
+
+    function getUVIndex() {
+        let UVIndexURL = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + lat + "&lon=" + lon + "&dt=" + dt + "&appid=" + apiKey;
+        fetch(UVIndexURL, {
+            method: 'GET',
+        })
+        .then( UVResponse => {
+            return UVResponse.json();
+        })
+        .then( UVData => {
+            console.log(UVData);
+
+            let uvi = UVData.current.uvi;
+            console.log(uvi);
+
+            let UVDisplay = $("<h3>").html(uvi)
+            weatherSection.append(UVDisplay);
+
+        })
+    }
+
+
+
+
+
+    weatherSection.prepend(weatherTitle);
+    weatherSection.append($("<img>").attr("src", iconURL));
+    weatherSection.append(temperatureDisplay);
+    weatherSection.append(humidityDisplay);
+    getUVIndex();
 });
 }
 
