208a8f4747c07b59faeb365716459b5956f579d0
BenBasic <benjaminmbasic@gmail.com>
2022-06-01 21:57:14 -0400

    Fixed search button, now correctly displays information based on searched city. Also added ability to add another button of previously searched cities in a list once searching a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let citySearch = "";
 let apiKey = "7eed36f27d06c72d7bd4346eee02a6cf";
 let searchEl = document.querySelector("#search-btn");
 let cityEl = document.querySelector("#search-city");
+let searchHistory = $('#search-history');
 
 function searchWeather() {
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&limit=5&appid=" + apiKey + "&units=metric";
@@ -20,11 +21,11 @@ function searchWeather() {
     let iconURL = "http://openweathermap.org/img/wn/" + icon + ".png";
     let weatherTitle = $("<h2>").html(citySearch + " (" + date + ")").attr({"class": "card-header"});
     let currentTemperature = data.main.temp;
-    let temperatureDisplay = $("<h2>").html(currentTemperature + " °C").attr({"class": "temp"});
+    let temperatureDisplay = $("<h2>").html("Temperature: " + currentTemperature + " °C").attr({"class": "temp"});
     let currentHumidity = data.main.humidity;
-    let humidityDisplay = $("<h2>").html(currentHumidity + "%").attr({"class": "humidity"});
+    let humidityDisplay = $("<h2>").html("Humidity: " + currentHumidity + "%").attr({"class": "humidity"});
     let currentWind = data.wind.speed;
-    let windDisplay = $("<h2>").html(currentWind + " km/h").attr({"class": "wind"});
+    let windDisplay = $("<h2>").html("Wind: " + currentWind + " km/h").attr({"class": "wind"});
     let lat = data.coord.lat;
     let lon = data.coord.lon;
     let dt = data.dt;
@@ -46,7 +47,7 @@ function searchWeather() {
             let uvi = UVData.current.uvi;
             console.log(uvi);
 
-            let UVDisplay = $("<h3>").html(uvi).attr({"class": "uvi"});
+            let UVDisplay = $("<h3>").html("UV Index: " + uvi).attr({"class": "uvi"});
             weatherSection.append(UVDisplay);
 
             if (uvi <= 2) {
@@ -74,7 +75,7 @@ function searchWeather() {
             let forecastDays = 1;
             
             for (i = 7; i < forecastData.list.length; i+=8) {
-                let forecastContainer = $('<section>').attr({"class": "card-p3"});
+                let forecastContainer = $('<section>').attr({"class": "card-p3 forecast-container"});
                 let forecastHeader = $('<h2>').attr({"class": "card-header"});
                 let forecastImageLink = "https://openweathermap.org/img/wn/" + forecastData.list[i].weather[0].icon + ".png";
                 let forecastImage = $('<img>').attr({"src": forecastImageLink});
@@ -118,7 +119,20 @@ function searchWeather() {
 });
 }
 
+function addHistory() {
+    if (!cities.includes(citySearch)) {
+        
+        cities.push(citySearch);
+        let historyBtn = $('<button>').attr({"class": "m-2"});
+        historyBtn.text(citySearch);
+        searchHistory.append(historyBtn);
+        localStorage.setItem('City History', JSON.stringify(cities));
+    }
+}
+
 $("#search-btn").click(function() {
+    event.preventDefault();
     citySearch = $("#search-city").val().trim();
+    addHistory();
     searchWeather();
 });
